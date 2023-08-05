33c21bbaca1702463b33e0974763fed143620cf1
Karina Kokhan <58032185+Karinaaaa23@users.noreply.github.com>
2023-08-05 13:20:32 +0300

    created the main struckture of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneralInformationComponent } from './general-information/general-information.component';
+import { AppTopBarComponent } from './app-top-bar/app-top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralInformationComponent,
+    AppTopBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/general-information/general-information.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-information',
+  templateUrl: './general-information.component.html',
+  styleUrls: ['./general-information.component.scss']
+})
+export class GeneralInformationComponent {
+
+}
